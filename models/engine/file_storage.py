7f86246892914fe33c66f0f3b935d00a9aec92a5
kai-dg <km.commissions@gmail.com>
2019-02-18 00:46:52 +0000

    created all() and new() functions on file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,4 +8,32 @@ from models.base_model import BaseModel
 class FileStorage:
     """FileStorage manages instances
     """
-    pass
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects
+        """
+
+        return self.__objects
+
+    def new(self, obj):
+        """Sets obj in __objects with format {}.{}
+        """
+
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[obj_key] = obj
+
+    def save(self):
+        """Serializes __objects into the JSON file
+        """
+
+        pass
+
+    def reload(self):
+        """Deserializes the JSON file if exists, otherwise do nothing
+        """
+
+        classes = {"BaseModel": BaseModel}
+        pass
