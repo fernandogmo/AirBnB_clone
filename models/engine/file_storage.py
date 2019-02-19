3ae37455185bda59d45608bb553fbcaceb54b4f3
kai-dg <km.commissions@gmail.com>
2019-02-19 05:44:39 +0000

    added FileStorage.reload(), updated base_model according to task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""FileStorage class
-"""
+"""FileStorage class"""
 import json
 from models.base_model import BaseModel
 
-class FileStorage():
+
+class FileStorage:
     """FileStorage manages instances
     """
 
@@ -45,22 +45,8 @@ class FileStorage():
         try:
             with open(self.__file_path, "r", encoding="utf-8") as r_file:
                 temp = json.load(r_file)
-        except:
+                for k, v in temp.items():
+                    temp_v = classes[v["__class__"]](**v)
+                    self.__objects[k] = temp_v
+        except FileNotFoundError:
             pass
-
-if __name__ == '__main__':
-    from models import storage
-    from models.base_model import BaseModel
-
-    all_objs = storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
-
-    print("-- Create a new object --")
-    my_model = BaseModel()
-    my_model.name = "Holberton"
-    my_model.my_number = 89
-    my_model.save()
-    print(my_model)
